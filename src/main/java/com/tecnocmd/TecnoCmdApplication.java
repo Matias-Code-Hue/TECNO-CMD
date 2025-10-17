95d3d984a995599c92f185055a46a5f5e55a77f3
Matias <matiasmoises2003@fpuna.edu.py>
2025-10-16 23:37:37 -0300

    Estructura inicial del Servidor

---
+++ src/main/java/com/tecnocmd/TecnoCmdApplication.java
@@ -0,0 +1,13 @@
+package com.tecnocmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecnoCmdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecnoCmdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnocmd/TecnoCmdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnocmd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecnoCmdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
